4ef522240a24bcf3f097691c80630a8061a38925
Roy Kokkelkoren <roy.kokkelkoren@gmail.com>
2015-12-28 15:41:49 +0100

    Finished integration test

--- test/integration/repository_flow_test.rb
+++ test/integration/repository_flow_test.rb
@@ -3,34 +3,44 @@ require 'test_helper'
 class RepositoryFlowTest < ActionDispatch::IntegrationTest
 
   test 'create repository and upload files with static values' do
-    open_session do |session|
 
-      title = 'Foobar12345'
-      pass = ')O(I*U&Y^T%R$E#W@Q!'
-      desc = 'This is a very long description'
+    title = 'Foobar12345'
+    pass = ')O(I*U&Y^T%R$E#W@Q!'
+    desc = 'This is a very long description'
 
-      https!
+    https!
 
-      get_via_redirect('/')
-      assert_template 'repositories/new'
+    get_via_redirect('/')
+    assert_template 'repositories/new'
 
-      assert_difference('Repository.count') do
-        post_via_redirect('/repositories', :repository => {:description => desc, :password => pass, :password_confirm => pass, :title => title})
-      end
-      assert_select 'h2#header', title
-      assert_select 'p.description', desc
-
-      repo_id = Repository.last.token
-
-      assert_difference('Record.count') do
-        put_via_redirect("/repositories/#{repo_id}/record/", :file => fixture_file_upload('test/fixtures/assets/foobar1.pdf','application/pdf'))
-      end
-
-      #assert_select 'table' do
-      #  assert_select 'tr' do
-      #    assert_select 'td:first', 'foobar1.pdf'
-      #  end
-      #end
+    assert_difference('Repository.count') do
+      post_via_redirect('/repositories', :repository => {:description => desc, :password => pass, :password_confirm => pass, :title => title})
     end
+    assert_select 'h2#header', title
+    assert_select 'p.description', desc
+    assert_select 'table td:nth-child(1)', 'No records'
+
+    repo = Repository.last
+    assert_difference('Record.count') do
+      put_via_redirect("/repositories/#{repo.token}/record/", :file => fixture_file_upload('test/fixtures/assets/foobar1.pdf','application/pdf'))
+    end
+    record = Record.last
+
+    get "/repositories/#{repo.token}"
+    @records = Record.where(repositories_id: repo.id)
+
+    assert_select 'table td:nth-child(1)', {:count => @records.size}
+    assert_select 'table td:nth-child(1)' do |cells|
+      cells.each do |cell|
+        assert_select cell, 'a', {:text => 'foobar1.pdf'}
+      end
+    end
+
+    assert_difference('Record.count', -1) do
+      delete_via_redirect("/repositories/#{repo.token}/record/#{record.token}")
+    end
+
+    get "/repositories/#{repo.token}"
+    assert_select 'table td:nth-child(1)', 'No records'
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,4 +11,8 @@ class ActiveSupport::TestCase
   # Load IP helper
   APP_CONFIG = YAML.load_file(Rails.root.join('config/config.yml'))
   IpHelper.createCIDR APP_CONFIG['IP_range']
+
+  def get_id_from_url(url)
+    url.split('/')[-1]
+  end
 end
