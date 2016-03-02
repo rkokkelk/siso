d2110612824ab4acaa3ef9d4b1da311ae465e43d
Roy Kokkelkoren <roy.kokkelkoren@gmail.com>
2016-03-03 00:15:44 +0100

    Webrick configured with TLS

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,8 @@ module Siso
 
     config.exceptions_app = self.routes
 
+    config.cache_store = :memory_store
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
