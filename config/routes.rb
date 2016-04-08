73986d7cd50977750aedb4415ed14ba0b8ef3f78
Roy Kokkelkoren <roy.kokkelkoren@gmail.com>
2016-04-08 09:20:56 +0200

    Changed order of routes due to 500 bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   root   'main#index'
 
+  # Error pages, first otherwise confused with repo
+  get '/404', :to => 'main#page_not_found'
+  get '/422', :to => 'main#unprocessable_entity'
+  get '/429', :to => 'main#retry_later'
+  get '/500', :to => 'main#server_error'
+
   # Repositories
   get    'new'                     => 'repositories#new'
   post   'new'                     => 'repositories#create'
@@ -14,10 +20,4 @@ Rails.application.routes.draw do
   get    ':id/:record_id'          => 'records#show'
   put    ':id/record'              => 'records#create'
   delete ':id/:record_id'          => 'records#delete'
-
-  # Error pages
-  get '/404', :to => 'main#page_not_found'
-  get '/422', :to => 'main#unprocessable_entity'
-  get '/429', :to => 'main#retry_later'
-  get '/500', :to => 'main#server_error'
 end
