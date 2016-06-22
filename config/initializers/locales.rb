2198badeea4ce59894c59d4d9447dd9ebb357339
Roy Kokkelkoren <roy.kokkelkoren@gmail.com>
2016-06-22 22:17:32 +0200

    Fixed configuration of Locales

--- config/initializers/locales.rb
+++ config/initializers/locales.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
-I18n.default_locale = Rails.configuration.x.config['LOCALES'] || :en
 I18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+I18n.default_locale = Rails.configuration.x.config['LOCALE'] || :en
